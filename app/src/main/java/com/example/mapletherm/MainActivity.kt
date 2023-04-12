2b88b2492819b635c376b77d721ab1fa48146a06
Gawquon <gawquonthestoryteller@gmail.com>
2023-04-12 01:32:06 -0400

    Scan Button UI

--- app/src/main/java/com/example/mapletherm/MainActivity.kt
+++ app/src/main/java/com/example/mapletherm/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.mapletherm
 
+import android.graphics.drawable.GradientDrawable.Orientation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import com.example.mapletherm.ui.theme.MapleThermTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +29,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Layout(resources.configuration.orientation)
                 }
             }
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Layout(orientation: Int)
+{
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        ScanButton(orientation)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    MapleThermTheme {
-        Greeting("Android")
+fun ScanButton(orientation: Int) {
+    Button(onClick =  {
+        // on-click code here, but first set up the activities
+    },
+    modifier = Modifier
+        .fillMaxWidth(0.95f)
+        .fillMaxHeight(0.20f)
+        .padding(horizontal = 5.dp)
+        .padding(top = 15.dp, bottom = 10.dp),
+    shape = RoundedCornerShape(30)
+    ) {
+        var fontSize = 10.em
+        if (orientation == 2) {
+            fontSize = 7.em
+        }
+        Text(text = "Start Scanning", fontSize = fontSize, color = Color.White)
     }
 }
