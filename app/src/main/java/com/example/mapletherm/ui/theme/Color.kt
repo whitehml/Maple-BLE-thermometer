9d1405023a15346122ac73a2ff73c24431564d54
Gawquon <gawquonthestoryteller@gmail.com>
2023-04-11 23:04:11 -0400

    first time setup

---
+++ app/src/main/java/com/example/mapletherm/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.mapletherm.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Orange = Color(0xFF773105)
+val OrangeGrey = Color(0xFF6F6053)
+val Fall = Color(0xFF977D68)
+
+val Red = Color(0xFF9C3939)
+val BrickRed = Color(0xFF543232)
+val Beige = Color(0xFFB38C8C)
